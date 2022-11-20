d4699fd862e95d7096d68ec9d4f58c7821e15d00
Diogo Correia <inlogic@gmail.com>
2022-11-20 14:00:08 +0000

    Add no alias test

--- main_test.go
+++ main_test.go
@@ -6,7 +6,132 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_generateOutline(t *testing.T) {
+func Test_generateOutline_withoutAlias(t *testing.T) {
+	src := `package app_test
+
+import (
+	"testing"
+)
+
+func TestFirst(a *testing.T) {
+	a.Run("First A", func(b *testing.T) {
+		b.Run("First A 1", func(c *testing.T) {
+			for i := 0; i < 10; i++ {
+				c.Run("First A 1 Alpha" + i, func(d *testing.T) {
+
+				})
+			}
+
+			c.Run("First A 1 Beta", func(d *testing.T) {
+
+			})
+		})
+
+		b.Run("First A 2", func(c *testing.T) {
+
+		})
+	})
+
+	a.Run("First B", func(b *testing.T) {
+		b.Run("First B 1", func(c *testing.T) {
+
+		})
+	})
+}
+
+func TestSecond(a *testing.T) {
+	a.Run("Second A", func(b *testing.T) {
+		b.Run("Second A 1", func(c *testing.T) {
+
+		})
+	})
+}
+`
+
+	tests, err := outline(src)
+	assert.NoError(t, err)
+	assert.Equal(t, []*Test{
+		{
+			Name:   "TestFirst",
+			Type:   TestType,
+			Path:   []string{},
+			LBrace: 42,
+			RBrace: 457,
+		},
+		{
+			Name:   "First A",
+			Type:   SubtestType,
+			Path:   []string{"TestFirst"},
+			LBrace: 79,
+			RBrace: 362,
+		},
+		{
+			Name:   "First A 1",
+			Type:   SubtestType,
+			Path:   []string{"TestFirst", "First A"},
+			LBrace: 119,
+			RBrace: 309,
+		},
+		{
+			Name:   "",
+			Type:   DynamicSubtestType,
+			Path:   []string{"TestFirst", "First A", "First A 1"},
+			LBrace: 192,
+			RBrace: 243,
+		},
+		{
+			Name:   "First A 1 Beta",
+			Type:   SubtestType,
+			Path:   []string{"TestFirst", "First A", "First A 1"},
+			LBrace: 259,
+			RBrace: 304,
+		},
+		{
+			Name:   "First A 2",
+			Type:   SubtestType,
+			Path:   []string{"TestFirst", "First A"},
+			LBrace: 319,
+			RBrace: 358,
+		},
+		{
+			Name:   "First B",
+			Type:   SubtestType,
+			Path:   []string{"TestFirst"},
+			LBrace: 371,
+			RBrace: 454,
+		},
+		{
+			Name:   "First B 1",
+			Type:   SubtestType,
+			Path:   []string{"TestFirst", "First B"},
+			LBrace: 411,
+			RBrace: 450,
+		},
+		{
+			Name:   "TestSecond",
+			Type:   TestType,
+			Path:   []string{},
+			LBrace: 459,
+			RBrace: 585,
+		},
+		{
+			Name:   "Second A",
+			Type:   SubtestType,
+			Path:   []string{"TestSecond"},
+			LBrace: 497,
+			RBrace: 582,
+		},
+		{
+			Name:   "Second A 1",
+			Type:   SubtestType,
+			Path:   []string{"TestSecond", "Second A"},
+			LBrace: 538,
+			RBrace: 578,
+		},
+	}, tests)
+}
+
+func Test_generateOutline_withAlias(t *testing.T) {
 	src := `package app_test
 
 import (
@@ -41,7 +166,7 @@ func TestFirst(a *alias.T) {
 
 func TestSecond(a *alias.T) {
 	a.Run("Second A", func(b *alias.T) {
-		b.Run("Second A 1", func(c *cenas.T) {
+		b.Run("Second A 1", func(c *alias.T) {
 
 		})
 	})
